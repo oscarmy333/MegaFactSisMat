48a4fe897cb50501d56c67a8b77288dcac6b8985
Grimaldo <gqpsystem@gmail.com>
2020-01-30 16:57:00 -0500

    first commit

---
+++ src/main/java/com/sistemas/MegaFactSisMatApplication.java
@@ -0,0 +1,13 @@
+package com.sistemas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaFactSisMatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaFactSisMatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemas/MegaFactSisMatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaFactSisMatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
